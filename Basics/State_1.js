e21d7c486482ea607755dd1f7d68613a48f5cb3c
NitinsG <36042347+NitinsG@users.noreply.github.com>
2020-05-16 00:26:45 +0530

    Create State_1.js

---
+++ Basics/State_1.js
@@ -0,0 +1,63 @@
+//Props cant be changed but State can be changed. For using state we have to use Class Component.
+//App.js
+
+import React from "react"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            answer: "Yes"
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>Is state important to know? {this.state.answer}</h1>
+                <ChildComponent answer={this.state.answer}/>      //Passing state to child component
+            </div>
+        )
+    }
+}
+
+export default App
+
+#Index.js
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
+
+//--------------------------------------------------------------------------
+
+import React from "react"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+    
+    render() {
+        let wordDisplay
+        if (this.state.isLoggedIn === true) {
+            wordDisplay = "in"
+        } else {
+            wordDisplay = "out"
+        }
+        return (
+            <div>
+                <h1>You are currently logged {wordDisplay}</h1>
+            </div>
+        )
+    }
+}
+
+export default App
+
